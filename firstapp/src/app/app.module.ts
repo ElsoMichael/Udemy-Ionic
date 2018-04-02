395b40374ae6d825668385099602928358ec30f9
ElsoMichael <elsomichael@ymail.com>
2018-04-02 07:00:18 -0400

    config merge

--- firstapp/src/app/app.module.ts
+++ firstapp/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
 import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
@@ -14,7 +13,6 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   declarations: [
     MyApp,
     ContactPage,
-    HomePage,
     TabsPage
   ],
   imports: [
@@ -25,7 +23,6 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     ContactPage,
-    HomePage,
     TabsPage
   ],
   providers: [
